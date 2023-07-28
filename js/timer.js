bec448c9223fa12cfc51b0e523fa3896802c1acf
Наташа Федорова <98808643+NatashaFedorova@users.noreply.github.com>
2023-07-29 01:37:13 +0300

    work about timer

--- js/timer.js
+++ js/timer.js
@@ -30,19 +30,44 @@ function getCurrentDate() {
 
   // Визначення дати наступного понеділка
   const nextMonday = new Date(currentDate);
+  const res = currentDate.getDate() + (8 - currentDayOfWeek);
+  console.log('res', res);
   nextMonday.setDate(currentDate.getDate() + (8 - currentDayOfWeek));
 
   // Вивід результатів у зручному форматі
-  console.log('Поточна дата:', currentDate.toDateString());
-  console.log('Попередній понеділок:', previousMonday.toDateString());
-  console.log('Наступний понеділок:', nextMonday.toDateString());
+  console.log('currentDate:', currentDate.getTime());
+  console.log('previousMonday:', previousMonday.getTime());
+  console.log('nextMonday:', nextMonday.getTime());
+  console.log(
+    'Наступний понеділок:',
+    nextMonday.toString().split(' ').slice(0, 4)
+  );
+
+  const passedSinceLastMonday =
+    currentDate.getTime() - previousMonday.getTime();
+  console.log('passedSinceLastMonday:', passedSinceLastMonday);
+
+  const passedDays = passedSinceLastMonday / 86400000;
+  console.log('passedDays:', passedDays);
+
+  const leftUntilNextMondayDays = nextMonday.getTime() - currentDate.getTime();
+  console.log('leftUntilNextMondayDays:', leftUntilNextMondayDays / 86400000);
+
+  const leftUntilNextMondayHours = nextMonday.getTime() - currentDate.getTime();
+  console.log('leftUntilNextMondayHours:', leftUntilNextMondayHours / 3600000);
+
+  const leftUntilNextMondayMinutes =
+    nextMonday.getTime() - currentDate.getTime();
+  console.log(
+    'leftUntilNextMondayMinutes:',
+    leftUntilNextMondayMinutes / 60000
+  );
 }
 
 //
 
 function timerDisplayInDays() {
   const value = refs.days.textContent;
-  //   console.log(value);
   //   refs.visualTimerDays.style.animationDuration = '2s';
 }
 
@@ -65,3 +90,8 @@ function timerDisplayInSeconds() {
 // в ср в 12:30 - 4 днів 11 год 30 хв 00 сек приблизно
 // коло для днів буде не повним в середині тижня, але повним на початку
 //  коло годин відображає число тільки від 1 до 23 в середині дня не повне
+
+// 1 sec = 1000ms
+// 60 sec or 1 min = 60000
+// 60 sec * 60 min = 3 600 000 sec/hour
+// 1 day = 86 400 000 sec
